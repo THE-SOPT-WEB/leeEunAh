e5f82e408ae9550de9a325ef68bbc2c5c5623261
aeuna <4294psy@naver.com>
2022-04-22 04:30:03 +0900

    feat: 이미 장바구니에 있는 버거 클릭시, 수량 변동

--- week2-2/main.js
+++ week2-2/main.js
@@ -1,4 +1,5 @@
 const $ = (selector) => document.querySelector(selector);
+const cartItemList = [];
 
 function attachCardClickEvent() {
   const breadCards = document.querySelectorAll('.bread__card');
@@ -19,24 +20,47 @@ function removeListItem(e) {
   clickedList.remove();
 }
 
-function addCartListItem(e) {
-  const breadCard = e.currentTarget;
-  const breadName = breadCard.querySelector('.bread__name').innerHTML;
-  const breadPrice = breadCard.querySelector('.bread__price').innerHTML;
+function checkItemInCart(breadName) {
+  return cartItemList.includes(breadName);
+}
 
+function plusAlreayItemNum(breadName) {
+  const cartList = $('.cart__list');
+  const alreadyCartItem = cartList.querySelector(`#${breadName}`);
+  const inputValue = +alreadyCartItem.querySelector('.cart__item-input').value + 1;
+  alreadyCartItem.querySelector('.cart__item-input').value = '' + inputValue;
+}
+
+function makeCartListItem(breadName, breadPrice) {
   const cartItem = document.createElement('li');
   cartItem.className = 'cart__item';
+  cartItem.id = `${breadName}`;
 
   cartItem.innerHTML = `<h4 class="cart__item-name">${breadName}</h4>
-  <input class="cart__item-input" value="1" type="number" />
-  <h4 class="cart__item-price">${calculatePriceToNumber(breadPrice).toLocaleString()}원</h4>
-  <button class="cart__item-button">X</button>`;
+    <input class="cart__item-input" value="1" type="number" />
+    <h4 class="cart__item-price">${calculatePriceToNumber(breadPrice).toLocaleString()}원</h4>
+    <button class="cart__item-button">X</button>`;
 
   const cartItemButton = cartItem.querySelector('.cart__item-button');
   cartItemButton.addEventListener('click', function (e) {
     removeListItem(e);
   });
+  return cartItem;
+}
 
+function addCartListItem(e) {
+  const breadCard = e.currentTarget;
+  const breadName = breadCard.querySelector('.bread__name').innerHTML;
+  const breadPrice = breadCard.querySelector('.bread__price').innerHTML;
+
+  if (checkItemInCart(breadName)) {
+    plusAlreayItemNum(breadName);
+    return;
+  } else {
+    cartItemList.push(breadName);
+  }
+
+  const cartItem = makeCartListItem(breadName, breadPrice);
   const cartList = $('.cart__list');
   cartList.appendChild(cartItem);
 }
