67f684783d5eb4ea16c27d19dcbc0e7b4d390874
aeuna <4294psy@naver.com>
2022-04-22 03:44:37 +0900

    feat: 카드 클릭시, 장바구니 요소 추가

--- week2-2/main.js
+++ week2-2/main.js
@@ -1,1 +1,36 @@
-console.log('test');
+const $ = (selector) => document.querySelector(selector);
+
+function attachCardClickEvent() {
+  const breadCards = document.querySelectorAll('.bread__card');
+  breadCards.forEach((breadCard) =>
+    breadCard.addEventListener('click', function (e) {
+      addCartListItem(e);
+    })
+  );
+}
+
+function calculatePriceToNumber(price) {
+  const removedComma = price.slice(0, -1).replace(/\D/g, '');
+  return +removedComma;
+}
+
+function addCartListItem(e) {
+  const breadCard = e.currentTarget;
+  const breadName = breadCard.querySelector('.bread__name').innerHTML;
+  const breadPrice = breadCard.querySelector('.bread__price').innerHTML;
+
+  const cartItem = document.createElement('li');
+  cartItem.className = 'cart__item';
+
+  cartItem.innerHTML = `<h4 class="cart__item-name">${breadName}</h4>
+  <input class="cart__item-input" value="1" type="number" />
+  <h4 class="cart__item-price">${calculatePriceToNumber(breadPrice).toLocaleString()}원</h4>
+  <button class="cart__item-button">X</button>`;
+
+  const cartList = $('.cart__list');
+  cartList.appendChild(cartItem);
+}
+
+window.onload = function () {
+  attachCardClickEvent();
+};
