23a4e7f95a5dd875787277ba54b29f109a9fa732
aeuna <4294psy@naver.com>
2022-04-22 18:05:33 +0900

    feat: 수량 변경 시, 가격 변경 추가

--- week2-2/main.js
+++ week2-2/main.js
@@ -10,11 +10,6 @@ function attachCardClickEvent() {
   );
 }
 
-function calculatePriceToNumber(price) {
-  const removedComma = price.slice(0, -1).replace(/\D/g, '');
-  return +removedComma;
-}
-
 function removeListItem(e) {
   const clickedList = e.currentTarget.parentNode;
   clickedList.remove();
@@ -31,6 +26,11 @@ function plusAlreayItemNum(breadName) {
   alreadyCartItem.querySelector('.cart__item-input').value = '' + inputValue;
 }
 
+function calculatePriceToNumber(price) {
+  const removedComma = price.slice(0, -1).replace(/\D/g, '');
+  return +removedComma;
+}
+
 function makeCartListItem(breadName, breadPrice) {
   const cartItem = document.createElement('li');
   cartItem.className = 'cart__item';
@@ -38,14 +38,31 @@ function makeCartListItem(breadName, breadPrice) {
 
   cartItem.innerHTML = `<h4 class="cart__item-name">${breadName}</h4>
     <input class="cart__item-input" value="1" type="number" />
-    <h4 class="cart__item-price">${calculatePriceToNumber(breadPrice).toLocaleString()}원</h4>
+    <h4 class="cart__item-price">${breadPrice}</h4>
     <button class="cart__item-button">X</button>`;
 
   const cartItemButton = cartItem.querySelector('.cart__item-button');
   cartItemButton.addEventListener('click', function (e) {
     removeListItem(e);
+    changeCartTotalPrice();
   });
+
+  const cartItemInput = cartItem.querySelector('.cart__item-input');
+  cartItemInput.addEventListener('change', function (e) {
+    changeCartTotalPrice();
+  });
+
   return cartItem;
+}
+
+function changeCartTotalPrice() {
+  const cartList = document.querySelectorAll('.cart__item');
+  let totalPrice = 0;
+  cartList.forEach(
+    (item) => (totalPrice += +item.querySelector('.cart__item-input').value * calculatePriceToNumber(item.querySelector('.cart__item-price').innerHTML))
+  );
+  const cartTotalPrice = $('.cart__total-price > h3');
+  cartTotalPrice.innerHTML = `${totalPrice.toLocaleString()}원`;
 }
 
 function addCartListItem(e) {
@@ -55,6 +72,7 @@ function addCartListItem(e) {
 
   if (checkItemInCart(breadName)) {
     plusAlreayItemNum(breadName);
+    changeCartTotalPrice();
     return;
   } else {
     cartItemList.push(breadName);
@@ -63,6 +81,7 @@ function addCartListItem(e) {
   const cartItem = makeCartListItem(breadName, breadPrice);
   const cartList = $('.cart__list');
   cartList.appendChild(cartItem);
+  changeCartTotalPrice();
 }
 
 window.onload = function () {
