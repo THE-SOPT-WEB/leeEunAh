fcfb8808585784012f2dd693a8f83f7c1f80e3f7
aeuna <4294psy@naver.com>
2022-05-13 03:22:44 +0900

    feat: Search 컴포넌트 추가

--- week4/src/App.js
+++ week4/src/App.js
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './global';
+import Search from './Search';
 import { theme } from './theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Search />
     </ThemeProvider>
   );
 }
